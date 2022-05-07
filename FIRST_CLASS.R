93a1c82a0438eca90773f16683a080972512eebd
qheney2019 <103369534+qheney2019@users.noreply.github.com>
2022-05-07 23:20:08 +0100

    Add files via upload

---
+++ FIRST_CLASS.R
@@ -0,0 +1,71 @@
+#data type(character, num, float, boelian) 
+
+country <- "nigeria"
+
+country
+
+str(country)
+
+name<- 'bola'
+
+str(name)
+
+scor<- 23
+
+str(scor)
+
+float <- 10.4 
+
+str(float)
+
+#naming
+
+#running commands
+
+
+# Create three variables with the values 12, "hello" and FALSE (you can name the variables whatever you like)
+numbers<- 12
+
+characters<- "hello"
+
+book <-" false"
+
+str(numbers)
+
+str(book)
+
+str(book)
+
+# Swap the values of the variables by changing the bindings
+
+numbers<- 34
+
+characters<- "dog"
+
+book<- "house"
+
+numbers
+
+characters
+
+# Create two numeric variables of your choice and 
+#(add, sub, multily, logic,)them together
+
+a<- 5
+b<- 10 
+
+add<- a + b
+
+add
+
+sub<- a-b
+
+sub 
+
+mult<- a * b
+
+mult
+ a>b
+a<b
+a==b # this is for comparison 
+a =b # this is an assign sign 
