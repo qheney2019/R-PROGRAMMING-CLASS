9d0ae60691fd11da1412e7964645a7d394997ba8
qheney2019 <103369534+qheney2019@users.noreply.github.com>
2022-05-09 00:26:05 +0100

    Add files via upload

---
+++ SECOND_CLASS.R
@@ -0,0 +1,76 @@
+#from the first class, !=, sentence.
+
+a<- 3
+b<- 4
+a!=b
+
+sen<- "my name is bola"
+
+sen
+
+
+#Create two character variables (strings) and combine them together(paste)
+
+#paste Function( concatination)
+
+school<- "usman danfodio university"
+state<- "sokoto state"
+
+paste(school, state)
+
+name<- "adetuji"
+my_name<- "my name is"
+
+
+paste(my_name, name )
+
+
+#another way to write the code
+
+"usman danfodio university"
+"my school is"
+paste("my school is", "usman danfofio university" )
+ 
+#rep function
+
+rep(name,3)
+
+rep(my_name, 5)
+
+#modulus.. 
+
+10%%3
+
+
+#conditional statements 
+# Write a conditional statement that prints "yes" if the value of a 
+# variable x is 3.
+
+x<-3
+if (x>5){
+  print("yes"){
+    
+  }else {
+    print ('no')
+  }
+  
+}
+
+  
+#Write a conditional statement that prints TRUE if a value is divisible
+# by 2, and FALSE otherwise.
+
+if (10%%3!=1){
+  print('TRUE')
+}else{
+  print('FALSE')
+}
+
+
+
+if (5<3){
+  print('TRUE')
+}else{
+  print('FALSE')
+}
+
