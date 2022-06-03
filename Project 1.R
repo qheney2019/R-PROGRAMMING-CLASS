79cc97d7264716be9988a8dbaa2f9f5a8034a547
qheney2019 <103369534+qheney2019@users.noreply.github.com>
2022-06-03 11:13:20 +0100

    Add files via upload

---
+++ Project 1.R
@@ -0,0 +1,10 @@
+# Create five (5) variables with the values 30, GOOD, 100, GREEN, and NIGERIA
+# (Name the variables with: number, status, century,colour and country respectively.)
+# write a comment about the code specify what type of data is each
+#create five(5) numerical variables with values: 50, 100, 150, 200, and 250
+#asign a variable name a-e for the five variable respectively
+#perform the following operations on the above variable: addition, subtraction,multiplication)
+#perform the following operation between variable A and B, (<, >, ==, %/%, %%, !=)
+#create three character variables: is a country, in Africa, Nigeria.
+#name the variable as: where, continent, country
+#write a code that return " Nigeria is a country in Africa.
