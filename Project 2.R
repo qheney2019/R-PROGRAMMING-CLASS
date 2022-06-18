bd6e5ca1b3c8c84fb3e0f8a5811413fdf4e49c73
qheney2019 <103369534+qheney2019@users.noreply.github.com>
2022-06-18 15:55:39 +0100

    Add files via upload

---
+++ Project 2.R
@@ -0,0 +1,28 @@
+#take note, indicate the question, write the code, and comment your answer
+
+# Write a conditional statement that prints "yes" if the value of a 
+# variable x is 5.
+
+# Write a conditional statement that prints TRUE if a value is divisible
+# by 2, and FALSE otherwise.
+
+#list a vector data of 10 variable, chose a data type of your choice
+#check the struture of the data set
+#bring out some of the variables of the data set(indexing),
+#change 2,3 and 4th variable to 15, 20, and 30.
+#append 20, 25, and 80 to the data set
+#create a list of 5 items with quantity you want to buy from supermarket
+#append the list with 3 more items
+#change the quantity of  two of the items
+#create a matrix of 5 rows, 5 coloumns 
+'''the cloumn name should be names of colours, and the rows should be
+abrreviation of name of the days'''
+#check the matrix
+#index some of the vriables
+#change 2  of the variable to something else
+#create an array of 2 rows, 3columns and 5 replicate
+#name the replicate the name of the month
+#index the array
+# write a read input function that return product of two numbers
+# you can include switch operator if you want 
+
